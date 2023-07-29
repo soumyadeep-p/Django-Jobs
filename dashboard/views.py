cfe5c632d7e5440ea95e54e648bd0d0c53bfc4a6
Dipit <dpatowari28@gmail.com>
2023-07-30 00:40:07 +0530

    minor change

--- dashboard/views.py
+++ dashboard/views.py
@@ -6,17 +6,4 @@ def dashboard(request):
         return render(request, 'dashboard/dashboard.html')
     else:
         messages.warning(request,'Please Log In to continue')
-        return redirect(request, 'login')
-# def proxy(request):
-#     if request.user.is_applicant:
-#         return redirect('applicant-dashboard')
-#     elif request.user.is_recruiter:
-#         return redirect('recruiter-dashboard')
-#     else:
-#         return redirect('login')
-    
-# def applicant_dashboard(request):
-#     return render(request, 'dashboard/applicant_dashboard.html')
-
-# def recruiter_dashboard(request):
-#     return render(request, 'dashboard/recruiter_dashboard.html')
+        return redirect('login')
